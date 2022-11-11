3d7278447e7818c34b19aaad4f64331bb945cda1
Felipe Nascimento Peixoto <peixoto.felipe19@gmail.com>
2022-11-11 17:07:03 -0300

    initial commit with finished exercises

---
+++ 4/script_4.js
@@ -0,0 +1,4 @@
+document.getElementById("btn-showName").addEventListener("click",function() {
+  const full_Name = document.getElementById('fullName').value;
+  alert("O nome completo é: " + full_Name);
+})
