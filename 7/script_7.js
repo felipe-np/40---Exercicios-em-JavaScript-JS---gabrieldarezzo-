ddbc85f70acd2dd53a9344d6d360fd3f59062194
Felipe Nascimento Peixoto <peixoto.felipe19@gmail.com>
2022-11-16 19:46:26 -0300

    Seventh exercise

---
+++ 7/script_7.js
@@ -0,0 +1,24 @@
+const lightOffSource = "https://raw.githubusercontent.com/gabrieldarezzo/helpjs-ravi/master/images/lampada.jpg";
+
+const lightOnSource = "https://raw.githubusercontent.com/gabrieldarezzo/helpjs-ravi/master/images/lampada-on.jpg";
+
+const lightBulbImg = document.querySelector('#light-bulb');
+
+// Alternative resolution using two events
+
+// lightBulbImg.addEventListener('mouseenter', function() {
+//   lightBulbImg.src = lightOnSource;
+// })
+
+// lightBulbImg.addEventListener('mouseleave', function(){
+//   lightBulbImg.src = lightOffSource;
+// })
+
+
+lightBulbImg.addEventListener('mouseover', function() {
+  if (lightBulbImg.src === lightOffSource) {
+    lightBulbImg.src = lightOnSource;
+  } else {
+    lightBulbImg.src = lightOffSource;
+  }
+})
