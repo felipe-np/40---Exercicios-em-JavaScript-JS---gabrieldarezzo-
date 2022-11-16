14607ccb09ae9f170cffe02bbc6e3210701d2e6b
Felipe Nascimento Peixoto <peixoto.felipe19@gmail.com>
2022-11-16 15:00:21 -0300

    second commit with new exercise

---
+++ 5/script_5.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("#btn-show-name");
+button.addEventListener("click", function() {
+  const fullName = document.querySelector("#full-name").value;
+  alert(`A quantidade de letras do seu nome é: ${fullName.length}`);
+})
