2b89437f469d435b4bf721a73943adcf1450040d
Felipe Nascimento Peixoto <peixoto.felipe19@gmail.com>
2022-11-23 12:11:42 -0300

    Updating exercise 16 and adding exercise 17.

---
+++ 17/script_17.js
@@ -0,0 +1,32 @@
+const numberOneInput = document.querySelector("#first-number");
+const numberTwoInput = document.querySelector("#second-number");
+const buttonSumInput = document.querySelector("#btn-sum");
+const resultEl = document.querySelector("#sum-result");
+
+numberOneInput.addEventListener('keyup', function() {
+  numberOneInput.value = formatNumber(numberOneInput.value);
+});
+
+numberTwoInput.addEventListener('keyup', function() {
+  numberTwoInput.value = formatNumber(numberTwoInput.value);
+});
+
+buttonSumInput.addEventListener('click', function() {
+  const firstValue = numberOneInput.value;
+  const secondValue = numberTwoInput.value;
+  
+  if (firstValue !== "" && secondValue !== "")  {
+    let sumOfValues = sum(firstValue, secondValue);
+    resultEl.innerHTML = `The result of the sum is: ${sumOfValues}`;
+  } else {
+    resultEl.innerHTML = `The result of the sum is 0`  ;
+  }
+});
+
+function formatNumber(value){
+  return value.replace(/\D/g, "");
+};
+
+function sum(firstValue, secondValue) {
+  return Number(firstValue) + Number(secondValue);
+};
