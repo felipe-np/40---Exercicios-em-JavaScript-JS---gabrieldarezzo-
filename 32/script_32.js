36a5721d172c1bee1b8a04b4c438be830b865589
Felipe Nascimento Peixoto <peixoto.felipe19@gmail.com>
2022-12-01 20:57:01 -0300

    Exercise 32.

---
+++ 32/script_32.js
@@ -0,0 +1,17 @@
+const gameInputEl = document.querySelector("#game-input");
+const pEl = document.querySelector("#result")
+gameInputEl.addEventListener("keyup" , function() {
+gameInputEl.value = formatText(gameInputEl.value);
+  
+  if (gameInputEl.value.toLowerCase().trim().indexOf("dota") > - 1) {
+    pEl.innerHTML = "You are correct!";
+  } else {
+  return pEl.innerHTML =  "You are incorrect!";
+}});
+
+function formatText(value) {
+value = value.replace(/[0-9]+/g, "");
+return value;
+}
+
+
